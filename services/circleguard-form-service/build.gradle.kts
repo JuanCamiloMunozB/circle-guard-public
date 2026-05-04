0a088f840a367b97ff43eb803553e52340418219
Juan Camilo Muñoz Barco <1105363626@u.icesi.edu.co>
2026-05-03 19:43:21 -0500

    feat: enhance kubernetes configurations and add health probes for services

--- services/circleguard-form-service/build.gradle.kts
+++ services/circleguard-form-service/build.gradle.kts
@@ -10,6 +10,7 @@ dependencies {
     implementation(platform("org.springframework.boot:spring-boot-dependencies:3.2.4"))
     testImplementation(platform("org.springframework.boot:spring-boot-dependencies:3.2.4"))
 
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.kafka:spring-kafka")
--- services/circleguard-notification-service/build.gradle.kts
+++ services/circleguard-notification-service/build.gradle.kts
@@ -9,6 +9,7 @@ dependencies {
     implementation(platform("org.springframework.boot:spring-boot-dependencies:3.2.4"))
     testImplementation(platform("org.springframework.boot:spring-boot-dependencies:3.2.4"))
 
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.kafka:spring-kafka")
     implementation("org.springframework.boot:spring-boot-starter-mail")
